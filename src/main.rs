dfa7fefc6b4895d6ce6404b7e823a4a89a9e6ab3
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-02-23 23:29:07 +0530

    added boolean code

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@ fn main() {
     println!("Hello nainasweetheart");
     let ans: u32 = sum(4, 5);
     println!("{}", ans);
+    println!("{}", is_even(33))
 }
 
 fn sum(a: u32, b: u32) -> u32 {
     return a + b;
+}
+
+// u32 , u64 , usize , u128  , i32 , i64 , i128
+
+fn is_even(a: u32) -> bool {
+    return a % 2 == 0;
 }
