be2a00abc16a74d4bc66db0edea5d23709124546
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-02-24 23:32:16 +0530

    studied loops in rust

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,39 @@
+// fn main() {
+//     println!("Hello nainasweetheart");
+//     let ans: u32 = sum(4, 5);
+//     println!("{}", ans);
+//     println!("{}", is_even(33));
+//     let name = "nainasweetheart"; // when you know name is immutable
+//     println!("{}", name);
+
+//     //second way
+
+//     let gf: String = String::from("Hazel");
+//     println!("my Ex girlfriend was : {}", gf);
+
+//     let arr: [i32; 5] = [1, 2, 3, 4, 5];
+//     println!("{:?}", arr.len());
+
+//     let vector = vec![1, 2, 3];
+//     println!("{:?}", vector);
+// }
+
+// fn sum(a: u32, b: u32) -> u32 {
+//     return a + b;
+// }
+
+// // u32 , u64 , usize , u128  , i32 , i64 , i128
+
+// fn is_even(a: u32) -> bool {
+//     return a % 2 == 0;
+// }
+
+// // let name = naina --> in case of js  in case of string there are two ways
+
+// loops in rust
+
 fn main() {
-    println!("Hello nainasweetheart");
-    let ans: u32 = sum(4, 5);
-    println!("{}", ans);
-    println!("{}", is_even(33));
-    let name = "nainasweetheart"; // when you know name is immutable
-    println!("{}", name);
-
-    //second way
-
-    let gf: String = String::from("Hazel");
-    println!("my Ex girlfriend was : {}", gf);
-
-    let arr: [i32; 5] = [1, 2, 3, 4, 5];
-    println!("{:?}", arr.len());
-
-    let vector = vec![1, 2, 3];
-    println!("{:?}", vector);
+    for i in 0..100 {
+        println!("{}", i);
+    }
 }
-
-fn sum(a: u32, b: u32) -> u32 {
-    return a + b;
-}
-
-// u32 , u64 , usize , u128  , i32 , i64 , i128
-
-fn is_even(a: u32) -> bool {
-    return a % 2 == 0;
-}
-
-// let name = naina --> in case of js  in case of string there are two ways
